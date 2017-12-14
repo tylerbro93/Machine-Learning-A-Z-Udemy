656280a564547a54a4ca8f61f48f436bb4b5c0af
Tyler <tylerbro54@yahoo.com>
2017-12-14 12:31:51 -0700

    Initial Commit

---
+++ Machine_Learning_AZ_Template_Folder/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/preprocessing.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv("Data.csv")
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+x[:, 0] = labelEncoder_X.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x = onehotencoder.fit_transform(x).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
